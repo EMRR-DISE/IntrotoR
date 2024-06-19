77d1b2f0efba831f6b27b905d7d7d5c138282479
Dave Bosworth <David.Bosworth@water.ca.gov>
2024-06-19 15:53:07 -0700

    Adding data for `bind_rows` and `left_join` examples and exercises

--- code/DataWrangling.R
+++ code/DataWrangling.R
@@ -1,7 +1,6 @@
 #let's figure out which dataset we want to use
 
 library(tidyverse)
-library(lubridate)
 
 # Battey, M. and S. Perry. 2023. Interagency Ecological Program: Discrete water quality
 # monitoring in the Sacramento-San Joaquin Bay-Delta, collected by the Environmental Monitoring Program,
@@ -29,11 +28,58 @@ ggplot(WQ_p8d7, aes(x = Station, y = Secchi)) + geom_boxplot()
 ggplot(WQ_p8d7, aes(x = Station, y = SpCndSurface)) + geom_boxplot()
 
 #it might be less intimidating if there were fewer columns.
+std_cols <- c(
+  "Station",
+  "Date",
+  "Chla",
+  "Pheophytin",
+  "TotAlkalinity",
+  "DissAmmonia",
+  "DissNitrateNitrite",
+  "DOC",
+  "TOC",
+  "DON",
+  "TotPhos",
+  "DissOrthophos",
+  "TDS",
+  "TSS",
+  "TKN",
+  "Depth",
+  "Secchi",
+  "Microcystis",
+  "SpCndSurface",
+  "WTSurface"
+)
 
-WQ_p8d7s = select(WQ_p8d7, Station, Date,
-                 Chla, Pheophytin, TotAlkalinity, DissAmmonia, DissNitrateNitrite, DOC, TOC,
-                 DON, TotPhos, DissOrthophos, TDS, TSS, TKN, Depth, Secchi, Microcystis,
-                 SpCndSurface, WTSurface)
-#export for later inporting
+WQ_p8d7s = select(WQ_p8d7, any_of(std_cols))
+#export for later importing
 
 write.csv(WQ_p8d7s, "data/WQ_P8D7.csv", row.names = F)
+
+# Export data from 2019 for P8 and D7 for bind_rows example
+WQ %>%
+  filter(year(Date) == 2019, Station %in% c("P8", "D7")) %>%
+  select(any_of(std_cols)) %>%
+  write_csv("data/WQ_2019.csv")
+
+# Export data from 2019-2022 for C3A for bind_rows exercise
+WQ %>%
+  filter(year(Date) %in% 2019:2022, Station == "C3A") %>%
+  select(any_of(std_cols)) %>%
+  write_csv("data/WQ_C3A_2019_2022.csv")
+
+# Export weather observation data from 2019-2022 for P8, D7, and C3A for left_join exercise
+WQ %>%
+  filter(year(Date) %in% 2019:2022, Station %in% c("P8", "D7", "C3A")) %>%
+  select(Station, Date, Weather, AirTemp, WindVelocity) %>%
+  write_csv("data/Weather_Obs_P8_D7_C3A_2019_2022.csv")
+
+# Dayflow data from 1997-2023 for left_join example
+df_dayflow <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/21c377fe-53b8-4bd6-9e1f-2025221be095/download/dayflow-results-1997-2023.csv")
+
+# Restrict to Delta Inflow and Outflow from 2019-2023 and export data
+df_dayflow %>%
+  transmute(Date = mdy(Date), Inflow = TOT, Outflow = OUT) %>%
+  filter(year(Date) >= 2019) %>%
+  write_csv("data/Dayflow_2019_2023.csv")
+
